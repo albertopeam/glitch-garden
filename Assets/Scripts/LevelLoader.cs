766addc12cf89184edc96d9a3baa94e5975abd3c
Alberto Penas Amor <alberto.penas@nomasystems.com>
2020-06-13 00:25:13 +0200

    created project & make splash screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0) {
+            StartCoroutine(LoadStartScene());
+        }         
+    }
+
+    private IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
